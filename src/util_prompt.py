acff12305d4be5505fb5b45f19dd5d2a970b3394
Luiz Anísio Vieira Batitucci <luizanisio@gmail.com>
2025-08-25 15:15:01 -0300

    Update util_prompt.py
    float temperatura

--- src/util_prompt.py
+++ src/util_prompt.py
@@ -171,7 +171,7 @@ class Prompt:
         gen_cfg = GENCONFIG.from_model_config(self._model.config)
         gen_cfg.max_new_tokens = 10
         gen_cfg.min_length = 1
-        gen_cfg.temperature = _temperatura
+        gen_cfg.temperature = float(_temperatura)
         gen_cfg.top_k = 20 if _temperatura > 0.3 else 2
         gen_cfg.do_sample = bool(_temperatura > 0.3)
         # predição
