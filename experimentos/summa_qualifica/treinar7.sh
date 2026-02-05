dc9c2f4be4f56ce821944e02035642dc4184fba1
Luiz Anísio <luizanisio@gmail.com>
2026-02-05 06:45:17 -0300

    treino qwen 7b

---
+++ experimentos/summa_qualifica/treinar7.sh
@@ -0,0 +1,8 @@
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+#if [ $# -eq 0 ]; then
+#    python ../../src/treinar_unsloth.py treinar_qwen15.yaml --info
+#else
+python ../../src/treinar_unsloth.py treinar_qwen7
+.yaml "$@"
+#fi
