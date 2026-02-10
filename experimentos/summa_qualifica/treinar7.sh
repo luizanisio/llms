89bec39aecb69122e53f4bcc2ff57e1734284487
Luiz Anisio <luizanisio@gmail.com>
2026-02-10 06:20:47 -0300

    ajustequebra linha

--- experimentos/summa_qualifica/treinar7.sh
+++ experimentos/summa_qualifica/treinar7.sh
@@ -3,6 +3,6 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 #if [ $# -eq 0 ]; then
 #    python ../../src/treinar_unsloth.py treinar_qwen15.yaml --info
 #else
-python ../../src/treinar_unsloth.py treinar_qwen7
-.yaml "$@"
+python ../../src/treinar_unsloth.py treinar_qwen7.yaml "$@"
+
 #fi
