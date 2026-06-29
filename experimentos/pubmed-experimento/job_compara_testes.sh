f13f9cd5b146c21e82c82a5091569f13f82a6fbf
Luiz Anísio <luizanisio@gmail.com>
2026-06-28 23:42:25 -0300

    chore: update conda environment to luizbat01 in job_compara_testes.sh

--- experimentos/pubmed-experimento/job_compara_testes.sh
+++ experimentos/pubmed-experimento/job_compara_testes.sh
@@ -43,7 +43,7 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR"
 
 source /opt/conda/etc/profile.d/conda.sh
-conda activate luizbat02
+conda activate luizbat01
 
 # echo "Configurando variáveis de ambiente..."
 # export CUDA_HOME=$CONDA_PREFIX
