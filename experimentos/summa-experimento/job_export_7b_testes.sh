c1ae7958a229709ab8a595cc869b6c7f8b4b4690
Luiz Anísio <luizanisio@gmail.com>
2026-09-08 14:43:58 -0300

    d1a e d1b

--- experimentos/summa-experimento/job_export_7b_testes.sh
+++ experimentos/summa-experimento/job_export_7b_testes.sh
@@ -61,7 +61,7 @@ nvidia-smi --query-gpu=name,memory.total,memory.free --format=csv,noheader 2>/de
 echo "==============================="
 
 
-PROTOCOLS=("b" "c" "d1" "d2" "d3" "d4" "d5" "d6" "d7" "d8" "d9" "d10" "d11" "d12" "d13" "d14" "d15" "d16" "d17" "d18" "d19" "d20" "d21" "d22" "d23" "d24" "d25")
+PROTOCOLS=("b" "c" "d1" "d1a" "d1b" "d2" "d3" "d4" "d5" "d6" "d7" "d8" "d9" "d10" "d11" "d12" "d13" "d14" "d15" "d16" "d17" "d18" "d19" "d20" "d21" "d22" "d23" "d24" "d25")
 
 for PROTOCOL in "${PROTOCOLS[@]}"; do
     CONFIG_FILE="05_extracao_${PROTOCOL}_teste.yaml"
