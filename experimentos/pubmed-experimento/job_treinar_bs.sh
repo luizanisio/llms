c838ee83dd520005b453e37a4e23f557f5081a11
Luiz Anísio <luizanisio@gmail.com>
2026-09-12 10:50:41 -0300

    treinar protocolos b

--- experimentos/pubmed-experimento/job_treinar_bs.sh
+++ experimentos/pubmed-experimento/job_treinar_bs.sh
@@ -4,7 +4,7 @@
 # =============================================================================
 
 # Nome do job — aparece no squeue e no nome dos arquivos de log (%x)
-#SBATCH --job-name=pubmed-treinar-d14
+#SBATCH --job-name=pubmed-treinar-bs
 
 # Partição de execução:
 #SBATCH --partition=gpu
@@ -19,7 +19,7 @@
 #SBATCH --mem=64G
 
 # Tempo máximo de execução (HH:MM:SS)
-#SBATCH --time=48:00:00
+#SBATCH --time=120:00:00
 
 # Arquivo de saída padrão: <job-name>_<job-id>.out
 #SBATCH --output=jobs_logs/%x_%j.out
@@ -57,10 +57,9 @@ nvidia-smi --query-gpu=name,memory.total,memory.free --format=csv,noheader 2>/de
 echo "==============================="
 
 CONFIGS=(
-  "04_treinar_d1.yaml"
-  "04_treinar_d2.yaml"
-  "04_treinar_d3.yaml"
-  "04_treinar_d4.yaml"
+  "04_treinar_b.yaml"
+  "04_treinar_b16.yaml"
+  "04_treinar_b16r8.yaml"
 )
 
 OUT_BASE="$BASE_DIR/treinos"
